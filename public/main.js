9c8ad165007fb632a1444f711553be0bf3f593f3
Delave-las-Kure <t1evgeny93@gmail.com>
2023-08-02 22:41:34 +0300

    fix refix

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ function init() {
     const iframe = document.querySelector('.uv-iframe')
     const address = location.pathname.replace(__uv$config.mainPrefix, '')
     const url = search(address.value, searchEngine.value);
-    const iframeSrc = __uv$config.mainPrefix + __uv$config.encodeUrl(url);
+    const iframeSrc = __uv$config.prefix + __uv$config.encodeUrl(url);
     iframe.src = iframeSrc
 }
 
