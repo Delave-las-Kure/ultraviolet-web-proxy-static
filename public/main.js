e533235cbc182b928420b553212cbb756464236c
Delave-las-Kure <t1evgeny93@gmail.com>
2023-08-02 22:43:41 +0300

    fix undefined address

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ function init() {
     const searchEngine = document.getElementById("uv-search-engine");
     const iframe = document.querySelector('.uv-iframe')
     const address = location.pathname.replace(__uv$config.mainPrefix, '')
-    const url = search(address.value, searchEngine.value);
+    const url = search(address, searchEngine.value);
     const iframeSrc = __uv$config.prefix + __uv$config.encodeUrl(url);
     iframe.src = iframeSrc
 }
