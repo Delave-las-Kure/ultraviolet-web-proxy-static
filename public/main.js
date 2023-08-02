8c8b6779e4d76a8668589cb6d1f335278f142a31
Delave-las-Kure <t1evgeny93@gmail.com>
2023-08-02 22:36:36 +0300

    fix: searchEngine

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 function init() {
+    const searchEngine = document.getElementById("uv-search-engine");
     const iframe = document.querySelector('.uv-iframe')
     const address = location.pathname.replace(__uv$config.mainPrefix, '')
     const url = search(address.value, searchEngine.value);
