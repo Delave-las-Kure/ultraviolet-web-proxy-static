cbfa4f7fc62197e176a6de3b4f3d360fe9783d1d
Delave-las-Kure <t1evgeny93@gmail.com>
2023-09-19 20:03:51 +0300

    up

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 function init() {
     const searchEngine = document.getElementById("uv-search-engine");
     const iframe = document.querySelector('.uv-iframe')
-    const address = location.pathname.replace(__uv$config.mainPrefix, '')
+    const address = __uv$config.decodeUrl(location.pathname.replace(__uv$config.mainPrefix, ''))
     const url = search(address, searchEngine.value);
-    const iframeSrc = __uv$config.prefix + url;
+    const iframeSrc = __uv$config.prefix + __uv$config.encodeUrl(url);
     iframe.src = iframeSrc
 }
 
