802dad9f59f609fa1b5371cd3e209efd6b1a789e
Delave-las-Kure <t1evgeny93@gmail.com>
2023-09-19 20:06:23 +0300

    up

--- public/index.js
+++ public/index.js
@@ -32,5 +32,5 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.mainPrefix + url;
+  location.href = __uv$config.mainPrefix + __uv$config.encodeUrl(url);
 });
