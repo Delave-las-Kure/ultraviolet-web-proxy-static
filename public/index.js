251eb66f8c378e3a1a6a83e638dbf5bc4d433b4c
Delave-las-Kure <t1evgeny93@gmail.com>
2024-11-08 19:58:13 +0300

    up

--- public/index.js
+++ public/index.js
@@ -21,8 +21,6 @@ const error = document.getElementById("uv-error");
 const errorCode = document.getElementById("uv-error-code");
 const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
 
-form.innerHTML = '';
-
 form.addEventListener("submit", async (event) => {
 	event.preventDefault();
 
